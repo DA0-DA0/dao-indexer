e057e445a7570ffe068c67dde4ccb40699843df9
gavin.doughtie <gavin.doughtie@gmail.com>
2022-03-19 09:30:59 -0700

    ugggh

--- src/historical_parser.rs
+++ src/historical_parser.rs
@@ -1,11 +1,27 @@
 use crate::db::models::{Block, NewBlock};
-use crate::indexer::tx::process_parsed;
+use crate::indexer::tx::{process_messages};
 use cosmrs::tx::Tx;
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
 use std::collections::HashSet;
 use tendermint_rpc::Client;
 use tendermint_rpc::HttpClient as TendermintClient;
+use tendermint::abci::responses::Event;
+use std::collections::BTreeMap;
+
+fn map_from_events(events: &Vec<Event>) -> BTreeMap::<String, Vec<String>> {
+    let mut event_map = BTreeMap::<String, Vec<String>>::default();
+    for event in events {
+        let mut event_strings = vec!();
+        for attribute in &event.attributes {
+            println!("discarding event attribute key {}", attribute.key.to_string());
+            event_strings.push(attribute.value.to_string())
+        }
+        event_map.insert(event.type_str.clone(), event_strings);
+    }
+    event_map
+}
+
 
 pub async fn block_synchronizer(
     db: &PgConnection,
@@ -44,21 +60,49 @@ pub async fn block_synchronizer(
                     .execute(db)
                     .expect("Error saving new Block");
             }
-            // TODO(gavindoughtie): get the events map here
-            for tx in response.block.data.iter() {
-                let unmarshalled_tx = Tx::from_bytes(tx.as_bytes()).unwrap();
-                let _ = process_parsed(db, &unmarshalled_tx, &None);
-                //process_tx_info(db, &unmarshalled_tx.tx_info, events);
-                for tx_message in unmarshalled_tx.body.messages {
-                    // TODO(jamesortega): Attach here gavins code to index based on the type of transaction
-                    classify_transaction(tx_message)
+            let results = tendermint_client.block_results(block_height as u32).await.unwrap();
+            if let Some(txs_results) = results.txs_results {
+                for tx in txs_results {
+                    println!("tx {:?} events: {:?}", tx, tx.events);
+                    let events = map_from_events(&tx.events);
+                    let unmarshalled_tx = Tx::from_bytes(tx.data.value()).unwrap();
+                    println!("unmarshalled_tx: {:?}", unmarshalled_tx);
+                    // let msg = tx.data;
+                    if !events.is_empty() {
+                        println!("Processed into event map: {:?}", events);
+                        let messages = vec!();
+                        let _ = process_messages(db, &messages, &Some(events));
+                    }
+                    // match Tx::from_bytes(tx.data.value()) {
+                    //     Ok(unmarshalled_tx) => {
+                    //         let _ = process_parsed(db, &unmarshalled_tx, &None);
+                    //         // //process_tx_info(db, &unmarshalled_tx.tx_info, events);
+                    //         // for tx_message in unmarshalled_tx.body.messages {
+                    //         //     // TODO(jamesortega): Attach here gavins code to index based on the type of transaction
+                    //         //     classify_transaction(tx_message)
+                    //         // }        
+                    //     },
+                    //     Err(e) => {
+                    //         eprintln!("Error from_bytes: {:?}", e);
+                    //     }
+                    // }
                 }
             }
+            // TODO(gavindoughtie): get the events map here
+            // for tx in response.block.data.iter() {
+            //     let unmarshalled_tx = Tx::from_bytes(tx.as_bytes()).unwrap();
+            //     let _ = process_parsed(db, &unmarshalled_tx, &None);
+            //     //process_tx_info(db, &unmarshalled_tx.tx_info, events);
+            //     for tx_message in unmarshalled_tx.body.messages {
+            //         // TODO(jamesortega): Attach here gavins code to index based on the type of transaction
+            //         classify_transaction(tx_message)
+            //     }
+            // }
         }
     }
 }
 
-fn classify_transaction(tx: cosmrs::Any) {
+pub fn classify_transaction(tx: cosmrs::Any) {
     match tx.type_url.to_string().as_str() {
         "/cosmwasm.wasm.v1.MsgInstantiateContract" => {
             println!("we found an instnatiate contract, p0g")
--- src/indexer/msg_instantiate_contract.rs
+++ src/indexer/msg_instantiate_contract.rs
@@ -15,6 +15,10 @@ impl Index for MsgInstantiateContract {
     db: &PgConnection,
     events: &Option<BTreeMap<String, Vec<String>>>,
   ) -> Result<(), Box<dyn std::error::Error>> {
+    if events.is_none() {
+      // TODO(gavindoughtie): Definitely NOT ok!
+      return Ok(());
+    }
     let contract_addresses = get_contract_addresses(events);
     let dao_address = contract_addresses.dao_address.as_ref().unwrap();
     let staking_contract_address = contract_addresses
@@ -26,20 +30,20 @@ impl Index for MsgInstantiateContract {
       let tx_height_strings = event_map.get("tx.height").unwrap();
       if !tx_height_strings.is_empty() {
         let tx_height_str = &tx_height_strings[0];
-        tx_height_opt = Some(BigDecimal::from_str(tx_height_str).unwrap());
+        tx_height_opt = Some(BigDecimal::from_str(tx_height_str)?);
       }
     }
     let tx_height: BigDecimal;
     if let Some(height) = tx_height_opt {
       tx_height = height;
     } else {
-      tx_height = BigDecimal::from_str("0").unwrap();
+      tx_height = BigDecimal::from_str("0")?;
     }
 
     let contract_model =
       NewContract::from_msg(dao_address, staking_contract_address, &tx_height, self);
     insert_contract(db, &contract_model);
-    let msg_str = String::from_utf8(self.msg.clone()).unwrap();
+    let msg_str = String::from_utf8(self.msg.clone())?;
     let instantiate_dao = serde_json::from_str::<Cw3DaoInstantiateMsg>(&msg_str)?;
     insert_dao(db, &instantiate_dao, &contract_addresses, Some(&tx_height))
   }
