dd1b7d3a52dc6f7a00135f04263a0544031011b1
gavin.doughtie <gavin.doughtie@gmail.com>
2022-03-26 14:00:07 -0700

    cleanup in historical parser

--- src/historical_parser.rs
+++ src/historical_parser.rs
@@ -40,44 +40,44 @@ pub async fn block_synchronizer(
     initial_block_height: u64,
     save_all_blocks: bool,
 ) {
-    if let Some(db) = &registry.db {
-        let tendermint_client = TendermintClient::new(tendermint_rpc_url).unwrap();
+    let db = registry.db.as_ref().unwrap();
 
-        let latest_block_response = tendermint_client.latest_block_results().await.unwrap();
-        let latest_block_height = latest_block_response.height.value();
+    let tendermint_client = TendermintClient::new(tendermint_rpc_url).unwrap();
 
-        for block_height in initial_block_height..latest_block_height {
-            let db_block_opt: Option<Block> = block
-                .find(block_height as i64)
-                .get_result::<Block>(db)
-                .optional()
-                .unwrap();
+    let latest_block_response = tendermint_client.latest_block_results().await.unwrap();
+    let latest_block_height = latest_block_response.height.value();
 
-            if db_block_opt.is_none() {
-                if block_height % 1000 == 0 {
-                    println!("Added another 1000 blocks, height: {}", block_height);
-                }
+    for block_height in initial_block_height..latest_block_height {
+        let db_block_opt: Option<Block> = block
+            .find(block_height as i64)
+            .get_result::<Block>(db)
+            .optional()
+            .unwrap();
 
-                let response = tendermint_client.block(block_height as u32).await.unwrap();
-                let block_hash = response.block_id.hash.to_string();
-                if save_all_blocks {
-                    let new_block = NewBlock::from_block_response(&block_hash, &response.block);
-                    diesel::insert_into(block)
-                        .values(&new_block)
-                        .execute(db)
-                        .expect("Error saving new Block");
-                }
+        if db_block_opt.is_none() {
+            if block_height % 1000 == 0 {
+                println!("Added another 1000 blocks, height: {}", block_height);
+            }
 
-                // Look at the transactions:
-                for tx in response.block.data.iter() {
-                    let tx_hash = tx_to_hash(tx);
-                    let tx_response = tendermint_client.tx(tx_hash, false).await.unwrap();
-                    let mut events = BTreeMap::<String, Vec<String>>::default();
-                    events.insert("tx.height".to_string(), vec![block_height.to_string()]);
-                    let _ = map_from_events(&tx_response.tx_result.events, &mut events);
-                    let unmarshalled_tx = Tx::from_bytes(tx.as_bytes()).unwrap();
-                    let _ = process_parsed(registry, &unmarshalled_tx, &Some(events));
-                }
+            let response = tendermint_client.block(block_height as u32).await.unwrap();
+            let block_hash = response.block_id.hash.to_string();
+            if save_all_blocks {
+                let new_block = NewBlock::from_block_response(&block_hash, &response.block);
+                diesel::insert_into(block)
+                    .values(&new_block)
+                    .execute(db)
+                    .expect("Error saving new Block");
+            }
+
+            // Look at the transactions:
+            for tx in response.block.data.iter() {
+                let tx_hash = tx_to_hash(tx);
+                let tx_response = tendermint_client.tx(tx_hash, false).await.unwrap();
+                let mut events = BTreeMap::<String, Vec<String>>::default();
+                events.insert("tx.height".to_string(), vec![block_height.to_string()]);
+                let _ = map_from_events(&tx_response.tx_result.events, &mut events);
+                let unmarshalled_tx = Tx::from_bytes(tx.as_bytes()).unwrap();
+                let _ = process_parsed(registry, &unmarshalled_tx, &Some(events));
             }
         }
     }
