c2b26c7cd9164d4d201264286b0912383d492900
gavin.doughtie <gavin.doughtie@gmail.com>
2022-03-26 13:10:44 -0700

    removed commented-out code

--- src/indexing/msg_execute_contract.rs
+++ src/indexing/msg_execute_contract.rs
@@ -14,34 +14,5 @@ impl Index for MsgExecuteContract {
         let msg_str = String::from_utf8(self.msg.clone())?;
         let msg_val: Value = serde_json::from_str(&msg_str)?;
         registry.index(events, &msg_val, &msg_str)
-        // let mut errors = vec![];
-        // // match serde_json::from_str::<Cw3DaoExecuteMsg>(&msg_str) {
-        // //     Ok(execute_contract) => {
-        // //         return execute_contract.index(registry, events);
-        // //     }
-        // //     Err(e) => {
-        // //         errors.push(e);
-        // //     }
-        // // };
-        // match serde_json::from_str::<StakeCw20ExecuteMsg>(&msg_str) {
-        //     Ok(execute_contract) => {
-        //         return execute_contract.index(registry, events);
-        //     }
-        //     Err(e) => {
-        //         errors.push(e);
-        //     }
-        // };
-        // // match serde_json::from_str::<Cw20ExecuteMsg>(&msg_str) {
-        // //     Ok(execute_contract) => {
-        // //         return execute_contract.index(registry, events);
-        // //     }
-        // //     Err(e) => {
-        // //         errors.push(e);
-        // //     }
-        // // }
-        // Err(Box::from(format!(
-        //     "could not interpret execute msg, got errors:\n{:?}",
-        //     errors
-        // )))
     }
 }
