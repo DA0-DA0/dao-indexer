f85bc49c8ce9be6bcfa65930f3f6f65d5e68f4a9
gavin.doughtie <gavin.doughtie@gmail.com>
2022-06-25 15:10:30 -0700

    working on persistence

--- src/db/db_mapper.rs
+++ src/db/db_mapper.rs
@@ -1,6 +1,7 @@
+use log::debug;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
-use std::collections::HashMap;
+use std::collections::{BTreeMap, HashMap};
 
 /// Relational mapping
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -36,7 +37,22 @@ pub struct FieldMapping {
     pub field_name: String,
     pub table_name: String,
     pub column_name: String,
-    // will probably need a policy defining HOW to put a value in a field
+    // TODO(gavindoughtie): will probably need a policy defining HOW to put a value in a field
+}
+
+impl FieldMapping {
+    pub fn new(message_name: &str, field_name: &str, table_name: &str, column_name: &str) -> Self {
+        FieldMapping {
+            message_name: message_name.to_string(),
+            field_name: field_name.to_string(),
+            table_name: table_name.to_string(),
+            column_name: column_name.to_string(),
+        }
+    }
+}
+
+trait Persister<T> {
+    fn save(&mut self, table_name: &str, column_name: &str, value: &Value, id: Option<T>) -> anyhow::Result<T>;
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -55,15 +71,18 @@ impl DatabaseMapper {
 
     // Add an inbound mapping FROM the message TO the database.
     pub fn add_mapping(
+        &mut self,
         message_name: &str,
         field_name: &str,
         table_name: &str,
         column_name: &str,
     ) -> anyhow::Result<()> {
-        println!(
+        debug!(
             "add_mapping(message_name: {}, field_name: {}, table_name: {}, column_name: {})",
             message_name, field_name, table_name, column_name
         );
+        let mapping = FieldMapping::new(message_name, field_name, table_name, column_name);
+        self.mappings.insert(message_name.to_string(), mapping);
         Ok(())
     }
 
@@ -102,4 +121,42 @@ impl Default for DatabaseMapper {
     fn default() -> Self {
         Self::new()
     }
+}
+
+type Record = BTreeMap<String, Value>;
+struct TestPersister {
+    pub tables: BTreeMap<String, HashMap<usize, Record>>,
+}
+
+impl TestPersister {
+    pub fn new() -> Self {
+        TestPersister {
+            tables: BTreeMap::new(),
+        }
+    }
+}
+
+impl Persister<usize> for TestPersister {
+    fn save(&mut self, table_name: &str, column_name: &str, value: &Value, id: Option<usize>) -> anyhow::Result<usize> {
+        let records: &mut HashMap<usize, Record> = self.tables
+            .entry(table_name.to_string())
+            .or_insert_with(|| {
+                HashMap::new()
+            });            
+        let id = match id {
+            Some(id) => id,
+            _ => records.len()
+        };
+
+        let record = records.entry(id).or_insert_with(|| BTreeMap::new());
+        record.insert(column_name.to_string(), value.clone());
+
+        Ok(id)
+    }
+}
+
+#[test]
+fn test_map_to_records() {
+  let persister = TestPersister::new();
+  persister.save("Table", "Column", Value::String::new("Test value"), None);
 }
