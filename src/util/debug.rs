7f93cdde5d70f499f9e681e8908546666aff0eab
James <11054922+entrancedjames@users.noreply.github.com>
2022-07-18 13:13:06 -0700

    fix clippy complaints

--- src/util/debug.rs
+++ src/util/debug.rs
@@ -1,6 +1,7 @@
 use crate::indexing::event_map::EventMap;
 use cw3_dao::msg::ExecuteMsg;
 use log::debug;
+use std::fmt::Write as _; // import without risk of name clashing
 
 pub fn dump_execute_contract(execute_contract: &ExecuteMsg) {
     debug!("handle execute contract {:?}", execute_contract);
@@ -10,7 +11,7 @@ pub fn events_string(events: &EventMap) -> String {
     let mut output = String::default();
     output.push_str("\n************* vv Events ***********\n");
     for (key, value) in events {
-        output.push_str(&format!("  {}: {:?}\n", key, value));
+        let _ = writeln!(output, "  {}: {:?}", key, value);
     }
     output.push_str("************* ^^ Events ***********\n");
     output
